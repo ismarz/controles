37e55531bc8760ba6e4e4b7beb6cb7cba0c0373f
Isma <ismarez78@gmail.com>
2022-02-06 21:15:47 +0100

    First commit

---
+++ calculadora.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  6 21:06:39 2022
+
+@author: Isma
+"""
+
+"""
+Calculadora Condicionales 
+
+1. Suma de 2 números
+    Introduce el primer número:
+                segundo número:
+    Resultado _+_= 
+2. Resta
+3. Multiplicación
+4. División
+    error: division by zero=> Indeterminación
+5. Cociente a//b
+6. Resto de la división a%b
+7. Exponención a**b pow(a,b)
+8. Salir
+"""
